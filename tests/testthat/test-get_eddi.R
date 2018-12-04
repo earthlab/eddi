0da36680fdfc5b03e13cd24842a04be4884298dc
Max Joseph <maxwellbjoseph@gmail.com>
2018-12-04 11:07:51 -0700

    use default arg for dir to use cache dir in test

--- tests/testthat/test-get_eddi.R
+++ tests/testthat/test-get_eddi.R
@@ -38,10 +38,9 @@ test_that("Timescale units other than 'week' or 'month' raise errors", {
 })
 
 test_that("Single dates return RasterStacks", {
-  r <- get_eddi(date = "2018-11-29", timescale = "1 month", dir = '.')
+  r <- get_eddi(date = "2018-11-29", timescale = "1 month")
   expect_is(r, "RasterStack")
   expect_equal(raster::nlayers(r), 1)
-  unlink(list.files(pattern = "*.asc"))
 })
 
 test_that("Multiple dates return RasterStacks", {
